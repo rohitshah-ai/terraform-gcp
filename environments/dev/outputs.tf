4a9545d0846866fe1bde5ef34daf83dac5ad54c3
rohitshah-ai <rohit.shah@nttdata.com>
2026-09-03 20:28:26 +0530

    Add VPC and subnet outputs to outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -13,3 +13,23 @@ output "artifact_registry" {
 output "cloud_sql_connection_name" {
   value = module.cloud_sql.connection_name
 }
+
+output "vpc_name" {
+  value = module.vpc.vpc_name
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "subnet_name" {
+  value = module.vpc.subnet_name
+}
+
+output "subnet_id" {
+  value = module.vpc.subnet_id
+}
+
+output "subnet_cidr" {
+  value = module.vpc.subnet_cidr
+}
